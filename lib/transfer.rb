1654e3ea7c5f144622b8fb45e82c4ff1ebdb0e86
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-03 18:58:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,8 @@ class Transfer
       @receiver.balance +=@amount
       @status = "complete"
     else
-      return "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
