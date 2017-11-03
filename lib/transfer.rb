2501de37ed40dc91a6becaf9523a8f593a45f7cb
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-03 18:52:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,8 +11,10 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
   def execute_transaction
-    @sender.balance -= @amount
-    @receiver.balance +=@amount
-    @status = "complete"
+    if @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance +=@amount
+      @status = "complete"
+    end  
   end
 end
